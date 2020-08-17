993950539039bb268f2b4bf8bab8dbe520bf1119
chenby <chenby@nextop.cn>
2020-08-17 16:38:25 +0800

    init

--- sub-module2/src/main/java/module-info.java
+++ sub-module2/src/main/java/module-info.java
@@ -3,6 +3,6 @@ import cn.nextop.sub.module2.ExampleInterfaceImpl2;
 
 module module2 {
 	exports cn.nextop.sub.module2;
-//	opens cn.nextop.sub.module2;
+	opens cn.nextop.sub.module2;
 	provides ExampleInterface with ExampleInterfaceImpl2;
 }
