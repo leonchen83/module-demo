7db92318a31c686c6ed93295f17a46a214d69796
chenby <chenby@nextop.cn>
2020-08-17 12:56:10 +0800

    init

--- run.sh
+++ run.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
-git pull
-export JAVA_HOME=/usr/local/jdk
-export M2_HOME=/home/chenby/apache-maven-3.5.0
-
-$M2_HOME/bin/mvn clean install
+mvn clean install
 rm -rf ./lib
 mkdir ./lib
 cp ./main-demo/target/main-demo-1.0-SNAPSHOT.jar ./lib
 cp ./sub-module1/target/sub-module1-1.0-SNAPSHOT.jar ./lib
 cp ./sub-module2/target/sub-module2-1.0-SNAPSHOT.jar ./lib
 
-$JAVA_HOME/bin/java --module-path lib --module main/cn.nextop.main.demo.Main
+java --module-path lib --module main/cn.nextop.main.demo.Main
