1e9fd2ca32349db97c4a1bf9885f73760bd87086
chenby <chenby@nextop.cn>
2020-08-17 12:51:42 +0800

    init

--- sub-module1/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-import cn.nextop.sub.module1.ExampleInterfaceImpl1;
-import cn.nextop.sub.module2.ExampleInterface;
-
-module module1 {
-	requires module2;
-//	requires transitive module2;
-	exports cn.nextop.sub.module1;
-	provides ExampleInterface with ExampleInterfaceImpl1;
-}
