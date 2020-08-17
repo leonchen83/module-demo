99148a9b078d9bc9f1a1e85661e0856d549c0840
leonchen83 <chen.bao.yi@qq.com>
2020-08-17 13:48:38 +0900

    init

--- run.sh
+++ run.sh
@@ -9,10 +9,5 @@ mkdir ./lib
 cp ./main-demo/target/main-demo-1.0-SNAPSHOT.jar ./lib
 cp ./sub-module1/target/sub-module1-1.0-SNAPSHOT.jar ./lib
 cp ./sub-module2/target/sub-module2-1.0-SNAPSHOT.jar ./lib
-lib_dir=./lib
-for file in "$lib_dir"/*.jar;
-do
-    CLASSPATH="$CLASSPATH":"$file"
-done
-#export CLASSPATH
-$JAVA_HOME/bin/java --module-path .lib --module main/cn.nextop.main.demo.Main
+
+$JAVA_HOME/bin/java --module-path lib --module main/cn.nextop.main.demo.Main
